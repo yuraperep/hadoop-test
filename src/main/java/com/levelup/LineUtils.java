18ced831c0e361007175e9f46c02aba2dabfa2c5
yuraperep <perep80@rambler.ru>
2013-11-25 20:12:22 +0200

    Work-1 Inner Object processing

--- src/main/java/com/levelup/LineUtils.java
+++ src/main/java/com/levelup/LineUtils.java
@@ -22,6 +22,10 @@ public class LineUtils {
     }
 
     public static Map<String, String> getFieldValueMap(String line) {
+        return getFieldValueMap(line,"");
+    }
+
+    public static Map<String, String> getFieldValueMap(String line,String keyPrefix) {
         String[] pairs = line.split(",");
         Map<String, String> values = new HashMap<>();
         for (String pair : pairs) {
@@ -29,14 +33,43 @@ public class LineUtils {
             if (keyValue.length == 2) {
                 String key = getValue(keyValue[0]);
                 if (key != null) {
-                    values.put(key, getValue(keyValue[1]));
+                    values.put(keyPrefix+key, getValue(keyValue[1]));
                 }
             }
         }
         return values;
     }
 
+//{ "uid" : "5768b8fe-cccb-4eeb-b52d-f7015d2aacb7" , "_id" : { "$oid" : "52617b7b26f4b3c367073fa9","time":"1382127311000"} , "__v" : 0}
+    public static Map<String, String> getKVwithInnerObjectsMap(String line) {
+
+        Map<String, String> values = new HashMap<>();
+
+        while(true){
+            processInnerObjects(values,line);
+        }
+
+        values.putAll(getFieldValueMap(line,""));
+        return values;
+    }
+
+    private static String processInnerObjects(Map<String, String> values, String line) {
+        int startOfSearch = line.indexOf("\"");
+        int startBrace = line.indexOf("{",startOfSearch);
+        int endBrace = line.indexOf("}",startBrace);
+        String beforePart = line.substring(0,startBrace);
+        int innerKeyStart = beforePart.lastIndexOf("\"[a-z0-9$_\\s]\"");
+        if(startBrace>-1 && endBrace>-1 && innerKeyStart>-1){
+            String innerObjectKey = beforePart.substring(innerKeyStart,beforePart.indexOf("\"",innerKeyStart));
+            String innerObject = line.substring(startBrace+1,endBrace);
+            values.putAll(getFieldValueMap(innerObject,innerObjectKey));
+            line = line.substring(0,innerKeyStart)+line.substring(endBrace);
+        }
+        break;
+    }
+
     private static String getValue(String s) {
+
         int startIndex = s.indexOf("\"");
         if (startIndex > -1) {
             String res = s.substring(startIndex + 1, s.lastIndexOf("\""));
