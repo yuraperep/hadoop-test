381ffbf3a463a56dca7b040ac1a463aa78730646
perep80 <perep80@rambler.ru>
2013-11-26 02:37:52 +0200

    Improve-7 Test templates

---
+++ src/test/java/com/levelup/FunctionalTest.java
@@ -0,0 +1,21 @@
+package com.levelup;
+
+/**
+ * User: perep80
+ * Time: 11/22/13 , 8:52 PM
+ * Project: hadoop-test
+ * Powered by IDEA
+ */
+public class FunctionalTest {
+
+    /**
+     *  for future possible FT:
+     *  1. load test files\data stored in project e.g. 50M file to input path
+     *  2. run sequentually hadoop tasks
+     *  3. after task end ?how find out? check output files results
+     *
+     * For this :
+     * - util class HDFS driver - to copy
+     *
+     */
+}
---
+++ src/test/java/com/levelup/MapReduceMockTest.java
@@ -0,0 +1,20 @@
+package com.levelup;
+
+/**
+ * User: perep80
+ * Time: 11/22/13 , 9:39 PM
+ * Project: hadoop-test
+ * Powered by IDEA
+ */
+public class MapReduceMockTest {
+
+    /**
+     *
+     * Create test with Map \ Reduce phase mocked
+     * for futher usage
+     *
+     *
+     *
+     */
+
+}
